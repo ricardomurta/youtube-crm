23d3f4454353ba2c6c24226098fe5201a4e107ae
Ricardo Murta <ricardo.murta.ext@sodexo.com>
2023-07-20 19:46:17 -0300

    Cálculos básicos. Exemplos alterados para produto.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-        byte idade; // -128 - 127 | 8-bit
-        short anoNascimento; // -32,768 - 32,767 | 16-bit
-        int idInteiro; // -2ˆ31 - (2ˆ31)-1 | 32-bit
-        long idLong; // -2ˆ63 - (2ˆ63)-1 | 64-bit
-        float salario; // 754 floating point | 32-bit
-        double indiceDeAtualizacao; // 754 floating point | 64-bit
+        byte diaFabricacao, mesFabricacao; // -128 - 127 | 8-bit
+        short anoFabricacao; // -32,768 - 32,767 | 16-bit
+        int pesoEmGramas; // -2ˆ31 - (2ˆ31)-1 | 32-bit
+        long codigoDoProduto; // -2ˆ63 - (2ˆ63)-1 | 64-bit
+        float valorUnitario; // 754 floating point | 32-bit
+        double margemDeLucro; // 754 floating point | 64-bit
         boolean ativo; // true and false | 1-bit
-        char genero; // Unicode's character | 16-bit
+        char categoria; // Unicode's character | 16-bit
 
-        idade = 35;
-        anoNascimento = 1988;
-        idInteiro = -1001;
-        idLong = 1001L;
-        salario = 15000F;
-        indiceDeAtualizacao = 8.1234567890D;
+        diaFabricacao = 14;
+        mesFabricacao = 7;
+        anoFabricacao = 2023;
+        pesoEmGramas = 32150;
+        codigoDoProduto = 1001100110011001100L;
+        valorUnitario = 1500.50F;
+        margemDeLucro = 20.1234567890D;
         ativo = true;
-        genero = 'M';
+        categoria = 'A';
 
-        System.out.println("Byte " + idade);
-        System.out.println("Short " + anoNascimento);
-        System.out.println("Integer " + idInteiro);
-        System.out.println("Long " + idLong);
-        System.out.println("Float " + salario);
-        System.out.println("Double " + indiceDeAtualizacao);
-        System.out.println("Aumento de salário " + salario * indiceDeAtualizacao / 100);
-        System.out.println("Salário atualizado " + (salario + salario * indiceDeAtualizacao / 100));
-        System.out.println("Boolean " + ativo);
-        System.out.println("Character " + genero);
+        System.out.println("Data de fabricação: " + diaFabricacao + "/" + mesFabricacao + "/" + anoFabricacao);
+        System.out.println("Peso: " + pesoEmGramas + "g");
+        System.out.println("Código do produto: " + codigoDoProduto);
+        System.out.println("Valor unitário: " + valorUnitario);
+        System.out.println("Margem de lucro: " + margemDeLucro);
+        System.out.println("Lucro: " + valorUnitario * margemDeLucro / 100);
+        System.out.println("Valor total: " + (valorUnitario + valorUnitario * margemDeLucro / 100));
+        System.out.println("Ativo? " + ativo);
+        System.out.println("Categoria: " + categoria);
     }
 }
