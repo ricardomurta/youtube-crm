5db8b589e9f50b991ca4208ce1398397618fa9f9
Ricardo Murta <ricardo.murta.ext@sodexo.com>
2024-02-18 20:24:58 -0300

    Arrays: productName;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,10 +13,12 @@ public class Main {
         o.unitValue = 199.99f;
         o.quantity = 10;
         o.totalValue = o.unitValue * o.quantity;
+        o.productName = new char[]{'I','p','h','o','n','e'};
 
         System.out.println("----------------------");
         System.out.println("Order date: " + o.date[2] + "/" + o.date[1] + "/" + o.date[0]);
         System.out.println("Order category: " + o.category);
+        System.out.println("Product name: " + Arrays.toString(o.productName));
         System.out.println("Product code: " + o.productCode);
         System.out.println("Product status:" + o.active);
         System.out.println("Product value: " + o.unitValue);
